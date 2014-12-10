9921aa11d119740393b106a85a605eb53ecf0434
eben-roux <me@ebenroux.co.za>
2014-12-10 07:40:46 +0200

    - release v1.0.2

--- Shuttle.Recall.Core/Properties/AssemblyInfo.cs
+++ Shuttle.Recall.Core/Properties/AssemblyInfo.cs
@@ -3,26 +3,26 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 #if NET35FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Core for .NET Framework 3.5")]
+[assembly: AssemblyTitle("Shuttle.Recall.Core for .NET Framework 3.5")]
 #endif
 
 #if NET40FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Core for .NET Framework 4.0")]
+[assembly: AssemblyTitle("Shuttle.Recall.Core for .NET Framework 4.0")]
 #endif
 
 #if NET45FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Core for .NET Framework 4.5")]
+[assembly: AssemblyTitle("Shuttle.Recall.Core for .NET Framework 4.5")]
 #endif
 
 #if NET451FULL
-[assembly: AssemblyTitle("Shuttle.ESB.Core for .NET Framework 4.5.1")]
+[assembly: AssemblyTitle("Shuttle.Recall.Core for .NET Framework 4.5.1")]
 #endif
 
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2014")]
 [assembly: AssemblyProduct("Shuttle.Recall")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
 [assembly: ComVisible(false)]
 
