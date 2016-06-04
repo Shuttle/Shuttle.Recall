583cb9870403b57e25b4840c20a0769a41132bf1
Eben <me@ebenroux.co.za>
2016-06-04 14:43:36 +0200

    - v3.6.1

--- Shuttle.Recall/EventProcessing/Pipeline/PipelineStateExtensions.cs
+++ Shuttle.Recall/EventProcessing/Pipeline/PipelineStateExtensions.cs
@@ -4,12 +4,12 @@ namespace Shuttle.Recall
 {
     public static class PipelineStateExtensions
     {
-        public static void SetWorking(this State<Pipeline> state)
+        public static void SetWorking(this IState<IPipeline> state)
         {
             state.Replace(StateKeys.Working, true);
         }
 
-        public static bool GetWorking(this State<Pipeline> state)
+        public static bool GetWorking(this IState<IPipeline> state)
         {
             return state.Get<bool>(StateKeys.Working);
         }
