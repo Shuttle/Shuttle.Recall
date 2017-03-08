e23f38cf130dfa3d518c14324efba4e129ae6997
Eben <me@ebenroux.co.za>
2017-03-08 21:59:01 +0200

    - changed RegisterComponents() to Register()

--- Shuttle.Recall/EventStore.cs
+++ Shuttle.Recall/EventStore.cs
@@ -86,7 +86,94 @@ namespace Shuttle.Recall
             }
         }
 
-        public static IEventStore Create(IComponentResolver resolver)
+		public static IEventStoreConfiguration Register(IComponentRegistry registry)
+		{
+			Guard.AgainstNull(registry, "registry");
+
+			var configuration = new EventStoreConfiguration();
+
+			new CoreConfigurator().Apply(configuration);
+
+			Register(registry, configuration);
+
+			return configuration;
+		}
+
+		public static void Register(IComponentRegistry registry, IEventStoreConfiguration configuration)
+		{
+			Guard.AgainstNull(registry, "registry");
+			Guard.AgainstNull(configuration, "configuration");
+
+			RegisterComponent<IEventStoreConfiguration>(registry, configuration);
+
+			RegisterComponent<ISerializer, DefaultSerializer>(registry);
+			RegisterComponent<IProjectionSequenceNumberTracker, ProjectionSequenceNumberTracker>(registry);
+			RegisterComponent<IPrimitiveEventQueue, PrimitiveEventQueue>(registry);
+
+			RegisterComponent<TransactionScopeObserver, TransactionScopeObserver>(registry);
+
+			if (!registry.IsRegistered<ITransactionScopeFactory>())
+			{
+				var transactionScopeConfiguration = configuration.TransactionScope ?? new TransactionScopeConfiguration();
+
+				RegisterComponent<ITransactionScopeFactory>(registry,
+					new DefaultTransactionScopeFactory(transactionScopeConfiguration.Enabled,
+						transactionScopeConfiguration.IsolationLevel,
+						TimeSpan.FromSeconds(transactionScopeConfiguration.TimeoutSeconds)));
+			}
+
+			RegisterComponent<IPipelineFactory, DefaultPipelineFactory>(registry);
+
+			var reflectionService = new ReflectionService();
+
+			foreach (var type in reflectionService.GetTypes<IPipeline>(typeof(EventStore).Assembly))
+			{
+				if (type.IsInterface || registry.IsRegistered(type))
+				{
+					continue;
+				}
+
+				registry.Register(type, type, Lifestyle.Transient);
+			}
+
+			foreach (var type in reflectionService.GetTypes<IPipelineObserver>(typeof(EventStore).Assembly))
+			{
+				if (type.IsInterface || registry.IsRegistered(type))
+				{
+					continue;
+				}
+
+				registry.Register(type, type, Lifestyle.Singleton);
+			}
+
+			registry.Register<IEventStore, EventStore>();
+			registry.Register<IEventProcessor, EventProcessor>();
+		}
+
+		private static void RegisterComponent<TDependency, TImplementation>(IComponentRegistry registry)
+			where TDependency : class
+			where TImplementation : class, TDependency
+		{
+			if (registry.IsRegistered(typeof(TDependency)))
+			{
+				return;
+			}
+
+			registry.Register<TDependency, TImplementation>();
+		}
+
+		private static void RegisterComponent<TDependency>(IComponentRegistry registry, TDependency instance)
+			where TDependency : class
+		{
+			if (registry.IsRegistered(typeof(TDependency)))
+			{
+				return;
+			}
+
+			registry.Register(instance);
+		}
+
+		public static IEventStore Create(IComponentResolver resolver)
         {
             Guard.AgainstNull(resolver, "resolver");
 
--- Shuttle.Recall/Properties/AssemblyInfo.cs
+++ Shuttle.Recall/Properties/AssemblyInfo.cs
@@ -25,11 +25,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Recall for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("4.0.44.0")]
+[assembly: AssemblyVersion("7.0.66.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2017")]
 [assembly: AssemblyProduct("Shuttle.Recall")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("4.0.44")]
+[assembly: AssemblyInformationalVersion("7.0.66")]
 [assembly: ComVisible(false)]
 
