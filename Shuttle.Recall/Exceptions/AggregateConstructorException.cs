d56398cd184db51e50d3f94112d2adbfefc4619a
eben-roux <me@ebenroux.co.za>
2018-08-07 19:24:22 +0200

    - added extension

---
+++ Shuttle.Recall/Exceptions/AggregateConstructorException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Shuttle.Recall
+{
+    public class AggregateConstructorException : Exception
+    {
+        public AggregateConstructorException()
+        {
+        }
+
+        public AggregateConstructorException(string message) : base(message)
+        {
+        }
+
+        public AggregateConstructorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AggregateConstructorException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
