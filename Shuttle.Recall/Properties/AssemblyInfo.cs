e6e5baf5e2861ad9e774ec02d5cf33ca08e369f2
Eben <me@ebenroux.co.za>
2024-05-03 15:58:13 +0200

    Package upgrade

--- Shuttle.Recall/Properties/AssemblyInfo.cs
+++ Shuttle.Recall/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard")]
 #endif
 
-[assembly: AssemblyVersion("17.0.0.0")]
+[assembly: AssemblyVersion("17.0.1.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2024, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Recall")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("17.0.0")]
+[assembly: AssemblyInformationalVersion("17.0.1")]
 [assembly: ComVisible(false)]
