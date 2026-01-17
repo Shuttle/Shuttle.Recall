a941ed6b722015b77050284b94cc5645924269e2
Eben <me@ebenroux.co.za>
2026-01-17 09:59:24 +0200

    EventStoreExtensions.

--- Shuttle.Recall/EventStoreExtensions.cs
+++ Shuttle.Recall/EventStoreExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Shuttle.Recall;
+﻿using Shuttle.Core.Contract;
+
+namespace Shuttle.Recall;
 
 public static class EventStoreExtensions
 {
@@ -6,22 +8,22 @@ public static class EventStoreExtensions
     {
         public async Task<EventStream> GetAsync(Action<EventStreamBuilder>? builder = null, CancellationToken cancellationToken = default)
         {
-            return await eventStore.GetAsync(Guid.Empty, builder, cancellationToken);
+            return await Guard.AgainstNull(eventStore).GetAsync(Guid.Empty, builder, cancellationToken);
         }
 
         public async Task<EventStream> GetAsync(CancellationToken cancellationToken = default)
         {
-            return await eventStore.GetAsync(Guid.Empty, null, cancellationToken);
+            return await Guard.AgainstNull(eventStore).GetAsync(Guid.Empty, null, cancellationToken);
         }
 
         public async Task<EventStream> GetAsync(Guid id, CancellationToken cancellationToken = default)
         {
-            return await eventStore.GetAsync(id, null, cancellationToken);
+            return await Guard.AgainstNull(eventStore).GetAsync(id, null, cancellationToken);
         }
 
         public async Task<IEnumerable<EventEnvelope>> SaveAsync(EventStream eventStream, CancellationToken cancellationToken = default)
         {
-            return await eventStore.SaveAsync(eventStream, null, cancellationToken);
+            return await Guard.AgainstNull(eventStore).SaveAsync(eventStream, null, cancellationToken);
         }
     }
 }
